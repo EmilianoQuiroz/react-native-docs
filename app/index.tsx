72d6b5603569bdf5245224693ae5f0c883fd2f2e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2025-08-02 18:39:07 +0100

    calculadora basica

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import Calculator from "@/components/Calculartor";
+import { Stack } from "expo-router";
+
+export default function Index() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Calculadora" }} />
+      <Calculator />
+    </>
+  );
+}
